831793c2154f7a81cc1932b66d589bb6db9a7a6e
matheus <viniciusomagnifico@hotmail.com>
2025-02-04 21:21:45 -0300

    arrumamos o carrrousel

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -3,9 +3,11 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; // Estilos principais do carousel
 import "slick-carousel/slick/slick-theme.css"; // Tema do carousel
 import "./styles.css"; // Estilos personalizados (opcional)
-import image1 from "../../assets/carrousel/banner1.jpg" 
-import image2 from "../../assets/carrousel/img2.webp" 
-import image3 from "../../assets/carrousel/img3.jpg" 
+import image1 from "../../assets/carrousel/banner1.jpg"
+import image2 from "../../assets/carrousel/img2.webp"
+import image3 from "../../assets/carrousel/img3.jpg"
+
+
 
 
 
@@ -32,21 +34,24 @@ function Carrousel() {
   return (
     <div className="carousel-container">
       <Slider {...settings}>
+
         <div className="carousel-slide">
           <div>
-
           </div>
           <img
             src={image1}
             alt="Slide 1"
           />
         </div>
+
         <div className="carousel-slide">
           <img
-           src={image2}
+            src={image2}
             alt="Slide 2"
           />
+
         </div>
+
         <div className="carousel-slide">
           <img
             src={image3}
